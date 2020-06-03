8852edf32b5b2b0db082d32890b77092f45a60dc
Efren Carvajal <carva97valvi@gmail.com>
2020-06-02 23:14:39 -0600

    Add comments to boot1.asm and Add Fix to MakeFile

--- Makefile
+++ Makefile
@@ -1,20 +1,25 @@
+ASMFLAGS = -f bin
+SOURCE = ./src/
+BIN = ./bin/
+
 bootdisk=disk.img
 blocksize=512
 disksize=100
 
-boot1=src/boot1
+BOOT1 = boot1
 
 # bootloader 2nd stage params
-boot2=src/boot2
+BOOT2 = boot2
 boot2pos= 1
 boot2size= 1
 
 # kernel params
-kernel=src/kernel
+KERNEL = kernel
 kernelpos= 3
 kernelsize= 6
 
-ASMFLAGS=-f bin
+
+
 file = $(bootdisk)
 
 all: clean mydisk boot1 write_boot1 boot2 write_boot2 kernel write_kernel hexdump launchqemu
@@ -23,28 +28,25 @@ mydisk:
 	dd if=/dev/zero of=$(bootdisk) bs=$(blocksize) count=$(disksize) status=noxfer
 
 boot1: 
-	nasm $(ASMFLAGS) $(boot1).asm -o $(boot1).bin 
+	nasm $(ASMFLAGS) $(SOURCE)$(BOOT1).asm -o $(BIN)$(BOOT1).bin 
 
 boot2:
-	nasm $(ASMFLAGS) $(boot2).asm -o $(boot2).bin
+	nasm $(ASMFLAGS) $(SOURCE)$(BOOT2).asm -o $(BIN)$(BOOT2).bin 
 
 kernel:
-	nasm $(ASMFLAGS) $(kernel).asm -o $(kernel).bin
+	nasm $(ASMFLAGS) $(SOURCE)$(KERNEL).asm -o $(BIN)$(KERNEL).bin
 
 write_boot1:
-	dd if=$(boot1).bin of=$(bootdisk) bs=$(blocksize) count=1 conv=notrunc status=noxfer
+	dd if=$(BIN)$(BOOT1).bin of=$(bootdisk) bs=$(blocksize) count=1 conv=notrunc status=noxfer
 
 write_boot2:
-	dd if=$(boot2).bin of=$(bootdisk) bs=$(blocksize) seek=$(boot2pos) count=$(boot2size) conv=notrunc status=noxfer
+	dd if=$(BIN)$(BOOT2).bin of=$(bootdisk) bs=$(blocksize) seek=$(boot2pos) count=$(boot2size) conv=notrunc status=noxfer
 
 write_kernel:
-	dd if=$(kernel).bin of=$(bootdisk) bs=$(blocksize) seek=$(kernelpos) count=$(kernelsize) conv=notrunc
+	dd if=$(BIN)$(KERNEL).bin of=$(bootdisk) bs=$(blocksize) seek=$(kernelpos) count=$(kernelsize) conv=notrunc
 
 hexdump:
 	hexdump $(file)
-
-disasm:
-	ndisasm $(boot1).asm
 
 launchqemu:
 	qemu-system-x86_64 -fda $(bootdisk)
--- src/boot1.asm
+++ src/boot1.asm
@@ -3,64 +3,66 @@
 ;   This is a modified code obtained at https://github.com/igormp/asm-x86-game
 ;   Author fbsouza
 ;   Created at Sep 6th 2018
+;	Modify By: Aaron Solera
+;	Modify By: Efren
 ;----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-BITS 16 ; informa ao assembler que eh um codigo de 16 bits, ou seja, no modo real
-org 0x7c00 ; endereco do bootsector area, apos utilizar a diretiva org deve-se carregar o CS e o DS com 0
-jmp 0x0000:_start ; carrega o DS com 0 atraves de um far jump
+BITS 16 			; tell the assembler that we're using 16 bit mode
+org 0x7c00 			; Sets the start addres
+jmp 0x0000:_start 	; charges the DS with 0 via a far jump
 
 _start:
-	mov ah, 00h
-	mov al, 03h ; inicia o modo texto
-	int 10h
+	mov ah, 0x00
+	mov al, 0x03 	; start the text mode
+	int 0x10
 
-	xor ax, ax ; zera o DS, pois a partir dele que o processador busca os dados utilizados no programa
+	xor ax, ax 		; zeroes the DS, because from it the processor seeks the data used in the program
 	mov ds, ax
 
-	mov si, msg     ; Print msg
+	mov si, msg     ; Loads the address of "msg" into SI register
 
 _print:
-	lodsb           ; AL=memory contents at DS:SI
-	cmp al, 0       ; If AL=0 then hang
-	je  _keypress
-	mov ah, 0Eh     ; Print AL
+	lodsb           ; Loads the current byte from SI into AL and increments the address in SI 
+	cmp al, 0       ; compares AL to zero
+	je  _keypress	; if AL == 0, jump to "_keypress"
+	mov ah, 0x0E    ; Set function 0xE
 	mov bx, 7
-	int 10h
-	jmp _print       ; Print next character
+	int 0x10		; Set the interrupt 0x10
+	jmp _print      ; Print next character
 
 _keypress:
-	mov ah, 01h     ; modo da chamada para keystroke
-    int 16h
-	mov ah, 00h
-    int 16h
-    cmp al, ' '
-    je  _reset
-	jmp _keypress
+	mov ah, 0x01	; Call mode for Key Stroke
+    int 0x16
+	mov ah, 0x00
+    int 0x16
+    cmp al, ' '		; compares AL to ' ' , fist step ask for press space space = ' '
+    je  _reset		; If AL ==  ' ' Jump to _reset
+	jmp _keypress	; Jump tp _keypress
 
 
 _reset:
-	mov ah, 0 ; AH = 0, codigo da funcao que reinicia o controlador de disco
-	mov dl, 0 ; numero do drive a ser resetado
-	int 13h
-	jc _reset ; caso aconteca algum erro, tenta novamente
+	mov ah, 0x0 	; AH = 0, function code that resets the disk controller
+	mov dl, 0 		; drive number to be reset
+	int 0x13
+	jc _reset 		; in case of error retry
 
-	mov ax, 0x50 ; ler o setor do endereco 0x500
-	mov es, ax ; segmento com dados extra
+	mov ax, 0x50 	; read the sector address 0x500
+	mov es, ax 		; 
 	xor bx, bx
 
 _read:
-	mov ah, 0x02 ; codigo da funcao que le do disco
-	mov al, 1 ; numero de setores a serem lidos
-	mov ch, 0 ; numero do cilindro a ser lido
-	mov cl, 2 ; numero do setor
-	mov dh, 0 ; numero do cabecote
-	mov dl, 0 ; numero do drive
-	int 13h
-	jc _read ; caso aconteca algum erro, tenta novamente
+	mov ah, 0x02 	; Function code for read from disk
+	mov al, 1 		; Number of sector to read
+	mov ch, 0 		; Number of cilinder to read
+	mov cl, 2 		; Sector Number
+	mov dh, 0 		; Head Number
+	mov dl, 0 		; Drive Number
+	int 0x13
+	jc _read 		; in case of error retry
 
-	jmp 0x50:0x0 ; executar o setor do endereco 0x500:0, vai para o boot2
+	jmp 0x50:0x0 	; Jump to address 0x500: 0, go to boot step two (boot2)
 
-	msg db "Welcome to Tanks Bootloader",10,13,"Press space to continue!",10,13,10,13,0
+msg: db "Welcome to Tanks Bootloader",10,13,"Press space to continue!",10,13,10,13,0 ; Message to Write on the Screen
 
-times 510-($-$$) db 0 ; boot tem que ter 512 bytes
-dw 0xAA55 ; assinatura do boot no final
+times 510-($-$$) db 0 ; Fill the output file with zeroes until 510 bytes are full 
+dw 0xAA55 			; Put the magic number that tells the BIOS this is bootable in the last two bits
